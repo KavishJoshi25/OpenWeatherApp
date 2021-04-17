4acd52a463f1fe8271bb9a262c95b9f80afcb494
Kavish Joshi <kjoshi@ivycomptech.com>
2021-04-17 21:15:50 +0530

    1. added tab view controller. 2. Maps integration

--- OpenWeatherApp/ViewController.swift
+++ OpenWeatherApp/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        appDelegate.locationArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
+        
+        return cell
+    }
 }
-
